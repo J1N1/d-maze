a4ba95a822037418387a30dc59692c592e08793a
kodylau <kodylau@f2fde065-22da-07a5-8c90-00d9169a1587>
2010-09-27 12:56:53 +0000

    More code.

--- trunk/src/maze/play/CurrentGameState.java
+++ trunk/src/maze/play/CurrentGameState.java
@@ -1,5 +1,13 @@
 package maze.play;
 
-public interface CurrentGameState {
+public class CurrentGameState {
+	public Pair[] location;
+	public int[] collected;
+	public int[][] grid;
 	
+	public CurrentGameState(Pair[] location, int[] collected, int[][] grid) {
+		this.location = location;
+		this.collected = collected;
+		this.grid = grid;
+	}
 }
